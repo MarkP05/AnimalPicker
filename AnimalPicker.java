11b7ba135828c747edced56f86a3792ecdf3963b
MarkP05 <mvpfister5@gmail.com>
2025-02-26 13:06:29 -0500

    Added cat and dog ASCII art when selection is made.

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -12,10 +12,17 @@ public class AnimalPicker {
 
             if (choice.equals("cat")) {
                 validChoice = true;
-                System.out.println("CAT PLACEHOLDER");
+                System.out.println(" |\\__/,|   (`\\\r\n" + //
+                                        " |_ _  |.--.) )\r\n" + //
+                                        " ( T   )     /\r\n" + //
+                                        "(((^_(((/(((_/");
             } else if (choice.equals("dog")) {
                 validChoice = true;
-                System.out.println("DOG PLACEHOLDER");
+                System.out.println("                                  .-.\r\n" + //
+                                        "    *\\___________________________()' `-,\r\n" + //
+                                        "     (   ______________________   /````\r\n" + //
+                                        "     //\\\\                      //\\\\\r\n" + //
+                                        "     \"\" \"\"                     \"\" \"\"");
             } else {
                 System.out.println("Error: Invalid option given.");
                 System.out.println("I'm not sure what that is, but it isn't a cat or a dog...");
